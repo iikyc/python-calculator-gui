2cc319ee6b2e0ddc1bf3dcb5b0e44c87f1e57d5f
RowanHaver <82335056+RowanHaver@users.noreply.github.com>
2021-04-11 22:35:13 +0400

    Add files via upload

--- calculatorGUI.py
+++ calculatorGUI.py
@@ -1,7 +1,10 @@
 import tkinter
+import threading
+import time
 
 root = tkinter.Tk()
 root.title("Calculator")
+root.resizable(0, 0)
 
 root.geometry("230x300")
 root.configure(bg="black")
@@ -28,6 +31,31 @@ def calculate():
             result = "error"
             expression = ""
     label_result.config(text=result)
+    expression = str(result)
+
+def key_handler(event):
+    global expression
+    if event.keysym in ("1","2","3","4","5","6","7","8","9","0"):
+        add(event.keysym)
+    elif event.keysym =="period":
+        add(".")
+    elif event.keysym == "plus":
+        add("+")
+    elif event.keysym == "minus":
+        add("-")
+    elif event.keysym == "asterisk":
+        add("*")
+    elif event.keysym == "slash":
+        add("/")
+    elif event.keysym in ("c","C"):
+        clear()
+    elif event.keysym in ("Return","equal"):
+        threading.Thread(target=calculate).start()
+    elif event.keysym == "BackSpace":
+        expression = expression[0:len(expression)-1]
+        label_result.config(text=expression)
+
+root.bind("<Key>", key_handler)
 
 # Create GUI
 label_result = tkinter.Label(root, text="0", bg="black", fg="white", font=('Helvetica', '20'))
@@ -46,42 +74,42 @@ button_divide = tkinter.Button(root, text="/",command=lambda: add("/"), height="
 button_divide.grid(row=1, column=3)
 
 button_4 = tkinter.Button(root, text="4", command=lambda: add("4"), height="3", width="6", font=('Helvetica', '15'))
-button_4.grid(row=2, column=0)
+button_4.grid(row=3, column=0)
 
 button_5 = tkinter.Button(root, text="5", command=lambda: add("5"), height="3", width="6", font=('Helvetica', '15'))
-button_5.grid(row=2, column=1)
+button_5.grid(row=3, column=1)
 
 button_6 = tkinter.Button(root, text="6", command=lambda: add("6"), height="3", width="6", font=('Helvetica', '15'))
-button_6.grid(row=2, column=2)
+button_6.grid(row=3, column=2)
 
 button_multiply = tkinter.Button(root, text="*", command=lambda: add("*"), height="3", width="6", font=('Helvetica', '15'))
-button_multiply.grid(row=2, column=3)
+button_multiply.grid(row=3, column=3)
 
 button_7 = tkinter.Button(root, text="7", command=lambda: add("7"), height="3", width="6", font=('Helvetica', '15'))
-button_7.grid(row=3, column=0)
+button_7.grid(row=4, column=0)
 
 button_8 = tkinter.Button(root, text="8", command=lambda: add("8"), height="3", width="6", font=('Helvetica', '15'))
-button_8.grid(row=3, column=1)
+button_8.grid(row=4, column=1)
 
 button_9 = tkinter.Button(root, text="9", command=lambda: add("9"), height="3", width="6", font=('Helvetica', '15'))
-button_9.grid(row=3, column=2)
-
-button_subtract = tkinter.Button(root, text="-", command=lambda: add("-"), height="3", width="6", font=('Helvetica', '15'))
-button_subtract.grid(row=3, column=3)
-
-button_clear = tkinter.Button(root, text="C", command=lambda: clear(), height="3", width="6", font=('Helvetica', '15'))
-button_clear.grid(row=4, column=0)
-
-button_0 = tkinter.Button(root, text="0", command=lambda: add("0"), height="3", width="6", font=('Helvetica', '15'))
-button_0.grid(row=4, column=1)
-
-button_dot = tkinter.Button(root, text=".", command=lambda: add("."), height="3", width="6", font=('Helvetica', '15'))
-button_dot.grid(row=4, column=2)
+button_9.grid(row=4, column=2)
 
 button_add = tkinter.Button(root, text="+", command=lambda: add("+"), height="3", width="6", font=('Helvetica', '15'))
 button_add.grid(row=4, column=3)
 
-button_equals = tkinter.Button(root, text="=", command=lambda: calculate(), height="3", width="6", font=('Helvetica', '15'))
-button_equals.grid(row=5, column=0, columnspan=4)
+button_clear = tkinter.Button(root, text="C", command=lambda: clear(), height="3", width="6", font=('Helvetica', '15'))
+button_clear.grid(row=5, column=0)
+
+button_0 = tkinter.Button(root, text="0", command=lambda: add("0"), height="3", width="6", font=('Helvetica', '15'))
+button_0.grid(row=5, column=1)
+
+button_dot = tkinter.Button(root, text=".", command=lambda: add("."), height="3", width="6", font=('Helvetica', '15'))
+button_dot.grid(row=5, column=2)
+
+button_subtract = tkinter.Button(root, text="-", command=lambda: add("-"), height="3", width="6", font=('Helvetica', '15'))
+button_subtract.grid(row=5, column=3)
+
+button_equals = tkinter.Button(root, text="=", command=lambda: threading.Thread(target=calculate).start(), height="3", width="26", font=('Helvetica', '15'))
+button_equals.grid(row=6, column=0, columnspan=4)
 
 root.mainloop()
